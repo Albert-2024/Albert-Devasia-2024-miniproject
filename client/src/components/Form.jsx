50f4ac6e6b47c11bea8eecf82fd72d0ff902fe51
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-08-11 14:11:43 +0530

    registration -2

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,78 +1,103 @@
 import React from 'react';
-import {TextInput, Input, Checkbox, Button, Group, Box} from '@mantine/core';
-import {useForm} from '@mantine/form';
+import { TextInput, Input, Checkbox, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { useId } from '@mantine/hooks';
 import { IMaskInput } from 'react-imask';
-import { Center } from '@mantine/core';
 import { Stack } from '@mantine/core';
 
 const Form = () => {
+//     const validPhone = (value) => {
+//         const isValid = /^\d{10}$/.test(value);
+//     }
+//     if (!isValid) {
+//       setPhoneNumberError('Please enter a valid phone number');
+//     } else {
+//       setPhoneNumberError('');
+//     }
+
+//     setPhoneNumber(value);
+//   };
     const form = useForm({
-      initialValues: {
-        email: '',
-        termsOfService: false,
-      },
-  
-      validate: {
-        email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-      },
+        
+        initialValues: {
+            email: '',
+            name: '',
+            phone: '',
+            termsOfService: false,
+        },
+
+        validate: {
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+            name: (value) => ((value.length > 2) ? null : 'Invalid name'),
+            phone: (value) => ((value.length > 9) ? null : 'Invalid phone')
+        }
     });
     const id = useId();
 
 
     return (
-       <Stack h={400} justify="space-around" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] })}>
-        <Box maw={400} mx="auto">
-            
-            
-                <div>
-            <form onSubmit={form.onSubmit((values) => console.log(values))}>
-            
-                <TextInput
-                mt="lg"
-                    placeholder="Your name"
-                    label="Full name"
-                    radius="md"
-                    withAsterisk
-                />
-            <TextInput
-                    mt="lg"
-                    withAsterisk
-                    label="Email"
-                    radius="md"
-                    placeholder="your@email.com"
-                    {...form.getInputProps('email')}
-                />
-               
-                <Input.Wrapper mt='md' id={id} label="Your phone" required maw={320} mx="auto">
-                   
-                    
-                 <Input
+        <Stack h={400} justify="space-around" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] })}>
+            <Box maw={400}  c="blue"mx="auto">
+
+
+                <div className='form' >
+                    <form style={{
+                        width: '400px',
+                        height: '350px',
+                    }}
+                        onSubmit={form.onSubmit((values) => console.log(values))}>
+
                         
-                        component={IMaskInput}
-                        mask="+7 (000) 000-00-00"
-                        id={id}
-                        radius="md"
-                        placeholder="Your phone"
-                    />
-                </Input.Wrapper>
+                        
+                        <TextInput
+                            mt="lg"
+                            placeholder="Your name"
+                            label="Full name"
+                            radius="md"
+                            withAsterisk
+                            {...form.getInputProps('name')}
+                        />
+                        <TextInput
+                            mt="lg"
+                            withAsterisk
+                            label="Email"
+                            radius="md"
+                            placeholder="your@email.com"
+                            {...form.getInputProps('email')}
+                        />
 
-                <Checkbox
-                    mt="md"
-                    label="I agree to sell my privacy"
-                    {...form.getInputProps('termsOfService', { type: 'checkbox' })}
-                />
+                        <Input.Wrapper mt='md' id={id}
+                            label="Your phone"
+                            
+                           >
 
-                <Group position="right" mt="md">
-                    <Button type="submit">Submit</Button>
-                </Group>
-            </form>
-         
-            </div>
-          
-            
-        </Box>
-       </Stack>
+                            <TextInput
+
+                                component={IMaskInput}
+                                mask="+91 00000 00000"
+                                id={id}
+                                radius="md"
+                                placeholder="Your phone"
+                                {...form.getInputProps('phone')}
+                            />
+                        </Input.Wrapper>
+
+                        <Checkbox
+                            mt="md"
+                            label="I agree to sell my privacy"
+                            {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+                        />
+
+                        <Group position="center" mt="md">
+                            <Button type="submit">Submit</Button>
+                        </Group>
+                    </form>
+
+                </div>
+
+
+            </Box>
+        </Stack>
     )
 }
 
