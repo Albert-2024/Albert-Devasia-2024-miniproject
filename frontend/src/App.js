c5ba48ec0d27dfd933fbc55e72a85a46a7dbc0f5
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-09-06 15:01:08 +0530

    ElectroCOm version 1 front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElement,
+  Route,
+  RouterProvider
+} from 'react-router-dom' 
 
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+  
 const App = () => {
   return (
-    <div>Welcome to ElectroCOM</div>
-  )
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+            <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App
